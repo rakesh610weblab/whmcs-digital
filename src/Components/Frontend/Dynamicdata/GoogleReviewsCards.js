716ced64bd1e70c204965b7e1eb8cc3e905f36f2
rakesh610weblab <rakesh.610weblab@gmail.com>
2022-03-02 18:22:38 +0530

    change

---
+++ src/Components/Frontend/Dynamicdata/GoogleReviewsCards.js
@@ -0,0 +1,38 @@
+const GoogleReviewsCards = [
+    {
+      id: 1,
+      image: "/images/user1.png",
+      title: "Team did great work in terms of integration",
+      description: "We were looking for an expert on eBay`s API quirks and nuances. WHMCS Digital team did great work in terms of integration. Very Detail-oriented",
+      author: "Scott Miller",
+      review: "5",
+    },
+
+    {
+      id: 2,
+      image: "/images/user1.png",
+      title: "Very happy with the services of these guys",
+      description: "WHMCS Digital provide high-quality services and the products are 100% genuine. I am using a custom theme built by WHMCS Digital for my web hosting business",
+      author: "Mickey Harris",
+      review: "5",
+    },
+    {
+      id: 3,
+      image: "/images/user1.png",
+      title: "I do recommend hiring them",
+      description: "Highly experienced team of experts having in-depth knowledge of WHMCS setup and integration with third party apps. My whole experience with the team was really awesome",
+      author: "Wilbur Jones",
+      review: "5",
+    },
+    {
+      id: 4,
+      image: "/images/user1.png",
+      title: "Very skilled team",
+      description: "We worked together on a server module development for WHMCS and I really appreciate the efforts of WHMCS Digital team. They are very skilled guys with up to date knowledge",
+      author: "Tomas Smith",
+      review: "5",
+    },
+  
+];
+  
+  export default GoogleReviewsCards;
